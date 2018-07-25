27acb553a58f3eea8f35ab10ad46ecf0deda8a63
Vyacheslav Lapin <Vyachesl@vLapin.ru>
2018-07-25 17:06:23 +0300

    You tube cutter

--- src/index.js
+++ src/index.js
@@ -3,120 +3,51 @@
 // https://www.youtube.com/embed/53pcd8gF2yk?start=60&end=90
 // https://youtu.be/53pcd8gF2yk?t=1m
 
-// function Cat(eyesColor, woolLength, tailLength) {
-//     this.eyesColor = eyesColor;
-//     this.woolLength = woolLength;
-//     this.tailLength = tailLength;
-//     //return this;
-// }
-// Cat.prototype.caress = function() { console.log("mrrrrr...") };
-// Cat.prototype.feed = function() { console.log("am-am!") };
-class Cat {
-    constructor(eyesColor, woolLength, tailLength) {
-        this.eyesColor = eyesColor;
-        this.woolLength = woolLength;
-        this.tailLength = tailLength;
-    }
+let s = "https://www.youtube.com/embed/";
+const sourceLink = prompt("Введите ссылку на видео",
+    "https://youtu.be/53pcd8gF2yk?t=1m");
 
-    // noinspection JSMethodCanBeStatic
-    caress() {
-        console.log("mrrrrr...")
-    }
+const arr = /https?:\/\/youtu\.be\/([^?]+)\?t?=?(.+)/
+    .exec(sourceLink);
 
-    // noinspection JSMethodCanBeStatic
-    feed() {
-        console.log("am-am!")
-    }
-}
+const hash = arr[1];
+const start = arr[2];
 
-const cat = {
-    eyesColor: "green",
-    woolLength: 10,
-    tailLength: 150,
-    caress: function() { console.log("mrrrrr..."); },
-    feed: function() { console.log("am-am!"); },
-};
+console.log(arr[0]);
+console.log(hash);
+console.log(start);
 
-// const cat = Object.create(Cat.prototype); //{__proto__: Cat.prototype}
-// Cat.call(cat, "green", 10, 150);
-const cat = new Cat("green", 10, 150);
+s += hash;
 
-// тесты:
-console.log(cat instanceof Cat); //true
-console.log(cat.eyesColor === "green"); // true
-console.log(cat.woolLength === 10); // true
-console.log(cat.tailLength === 150); // true
-cat.caress(); // mrrrrr...
-cat.feed(); // am-am!
+const regExp = /(\d+h)?(\d+m)?(\d+s)?/;
+const timeArr = regExp.exec(start);
+const startHours = timeArr[1] === undefined ? 0 : parseInt(timeArr[1]);
+const startMinutes = timeArr[2] === undefined ? 0 : parseInt(timeArr[2]);
+const startSeconds = timeArr[3] === undefined ? 0 : parseInt(timeArr[3]);
 
-// function PersianCat(eyesColor, woolLength, tailLength) {
-//     // Cat.call(this, eyesColor, woolLength, tailLength);
-//     Cat.apply(this, arguments);
-// }
-class PersianCat extends Cat {
-    constructor(eyesColor, woolLength, tailLength) {
-        super(eyesColor, woolLength, tailLength);
-    }
+s += "?start=" + (startSeconds +
+    (startMinutes +
+        (startHours * 60)) * 60);
 
-    // noinspection JSMethodCanBeStatic
-    combOut() {
-        console.log("Myau!")
-    }
-}
+console.log(startHours);
+console.log(startMinutes);
+console.log(startSeconds);
 
-// Вариант №1 (ECMAScript 3)
-// PersianCat.prototype = new Cat(/*...*/);
+const end = prompt(`Время окончания фрагмента (должно быть больше, чем ${start})\n, можно оставить пустым`);
+const endTimeArr = regExp.exec(end);
+const endHours = endTimeArr[1] === undefined ? 0 : parseInt(endTimeArr[1]);
+const endMinutes = endTimeArr[2] === undefined ? 0 : parseInt(endTimeArr[2]);
+const endSeconds = endTimeArr[3] === undefined ? 0 : parseInt(endTimeArr[3]);
 
-// Вариант №2 (ECMAScript 5)
-// PersianCat.prototype = Object.create(Cat.prototype);
-// PersianCat.prototype.constructor = PersianCat;
+console.log(endHours);
+console.log(endMinutes);
+console.log(endSeconds);
 
-// Вариант №3 (ECMAScript 2015)
-// Object.setPrototypeOf(PersianCat.prototype, Cat.prototype);
+s += "&end=" + (endSeconds +
+    (endMinutes +
+        (endHours * 60)) * 60);
 
-// PersianCat.prototype.combOut = function () {
-//     console.log("Myau!")
-// };
+if (confirm("Хочешь autoplay?"))
+    s += "&autoplay=1";
 
-const persianCat = new PersianCat("gray", 150, 50);
-
-// тесты:
-console.log(persianCat instanceof Cat); //true
-console.log(persianCat instanceof PersianCat); //true
-console.log(persianCat.eyesColor === "gray"); // true
-console.log(persianCat.woolLength === 150); // true
-console.log(persianCat.tailLength === 50); // true
-persianCat.caress(); // mrrrrr...
-persianCat.feed(); // am-am!
-persianCat.combOut(); // Myau!
-
-// function SphinxCat(eyesColor, tailLength) {
-//     Cat.call(this, eyesColor, 0, tailLength);
-// }
-//
-// Object.setPrototypeOf(SphinxCat.prototype, Cat.prototype);
-
-class SphinxCat extends Cat {
-    constructor(eyesColor, tailLength) {
-        super(eyesColor, 0, tailLength);
-    }
-}
-
-const sphinxCat = new SphinxCat("yellow", 100);
-
-// тесты:
-console.log(sphinxCat instanceof Cat); //true
-console.log(sphinxCat instanceof SphinxCat); //true
-console.log(sphinxCat.eyesColor === "yellow"); // true
-console.log(sphinxCat.woolLength === 0); // true
-console.log(sphinxCat.tailLength === 100); // true
-sphinxCat.caress(); // mrrrrr...
-sphinxCat.feed(); // am-am!
-
-// function instanceOf(obj, Constructor) {
-//     if (obj === null)
-//         return false;
-//     const prototype = Object.getPrototypeOf(obj); // obj.__proto__
-//     return Constructor.prototype === prototype ? true :
-//         instanceOf(prototype, Constructor);
-// }
+prompt("Вот она ваша ссылка:", s);
