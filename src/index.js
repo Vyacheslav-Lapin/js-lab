cea3afb29cb172992b7f7eb094f930e45fa6e5ad
Vyacheslav Lapin <Vyachesl@vLapin.ru>
2018-07-26 13:09:33 +0300

    JSON parsing from file

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@
 // https://www.youtube.com/embed/53pcd8gF2yk?start=60&end=90
 // https://youtu.be/53pcd8gF2yk?t=1m
 // javascript:p=prompt,t=c=>{let d=x=>x?parseInt(x):0,[,h,m,s]=/(\d+h)?(\d+m)?(\d+s)?/i.exec(c);return(d(h)*60+d(m))*60+d(s)},[,h,s]=/https?:\/\/youtu\.be\/([^?]+)\?t?=?(.+)/i.exec(p("Введите ссылку на видео")),e=p(`Время окончания фрагмента (должно быть больше, чем ${s})\n, можно оставить пустым`);void p("Вот она ваша ссылка:", `https://www.youtube.com/embed/${h}?start=${t(s)}&end=${t(e)}&autoplay=1`)
-// import fs from 'fs';
+import fs from 'fs';
+import path from 'path';
 
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
 
 const filePath = path.join(__dirname, '../package.json');
 
