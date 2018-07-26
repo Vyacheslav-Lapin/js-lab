8f04876b32e5516b5d34e51ac72b634065ec9b45
Vyacheslav Lapin <Vyachesl@vLapin.ru>
2018-07-26 17:26:53 +0300

    try/catch & timeout/interval demo

--- src/index.js
+++ src/index.js
@@ -3,34 +3,24 @@
 // https://www.youtube.com/embed/53pcd8gF2yk?start=60&end=90
 // https://youtu.be/53pcd8gF2yk?t=1m
 // javascript:p=prompt,t=c=>{let d=x=>x?parseInt(x):0,[,h,m,s]=/(\d+h)?(\d+m)?(\d+s)?/i.exec(c);return(d(h)*60+d(m))*60+d(s)},[,h,s]=/https?:\/\/youtu\.be\/([^?]+)\?t?=?(.+)/i.exec(p("Введите ссылку на видео")),e=p(`Время окончания фрагмента (должно быть больше, чем ${s})\n, можно оставить пустым`);void p("Вот она ваша ссылка:", `https://www.youtube.com/embed/${h}?start=${t(s)}&end=${t(e)}&autoplay=1`)
-import fs from 'fs';
-import path from 'path';
 
-// const fs = require('fs');
-// const path = require('path');
-
-const filePath = path.join(__dirname, '../package.json');
-
-// console.log(filePath);
-
-fs.readFile(filePath, {encoding: 'utf-8'}, (err, data) => {
-    if (!err) {
-        console.log(`received data: ${data}`);
-
-        // JSON parsing
-        const json = JSON.parse(data);
-        const {
-            devDependencies: {"babel-cli": babelCli},
-            scripts: {run}
-        } = json;
-        console.log(babelCli);
-        console.log(run);
-
-        // File lines read
-        const lines = data.split('\n');
-        for (let line of lines) {
-            console.log(line.substr(0, 15));
-        }
+try {
+    if (Math.round(Math.random() * 2) % 2 === 1) {
+        console.log("one");
+        eval("khjsgdfjkhgas");
     } else
-        console.log(err);
-});
+        throw new Error("jkhsdkjfg");
+} catch (e) {
+    console.log(e);
+}
+
+const timeout = setTimeout(() => console.log("jhsdf"), 2000);
+console.log(timeout);
+console.log("qwerty");
+clearTimeout(timeout);
+//...
+const interval = setInterval(() => console.log("qwerty"), 2000);
+console.log(interval);
+
+const x = setInterval(() => {
+    console.log(x);clearInterval(x)}, 2000);
