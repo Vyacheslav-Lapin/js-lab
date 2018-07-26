8a462716fb38a8ef5afb66b20571e3da55f39dc7
Vyacheslav Lapin <Vyachesl@vLapin.ru>
2018-07-26 13:06:24 +0300

    JSON parsing from file

--- src/index.js
+++ src/index.js
@@ -3,13 +3,33 @@
 // https://www.youtube.com/embed/53pcd8gF2yk?start=60&end=90
 // https://youtu.be/53pcd8gF2yk?t=1m
 // javascript:p=prompt,t=c=>{let d=x=>x?parseInt(x):0,[,h,m,s]=/(\d+h)?(\d+m)?(\d+s)?/i.exec(c);return(d(h)*60+d(m))*60+d(s)},[,h,s]=/https?:\/\/youtu\.be\/([^?]+)\?t?=?(.+)/i.exec(p("Введите ссылку на видео")),e=p(`Время окончания фрагмента (должно быть больше, чем ${s})\n, можно оставить пустым`);void p("Вот она ваша ссылка:", `https://www.youtube.com/embed/${h}?start=${t(s)}&end=${t(e)}&autoplay=1`)
+// import fs from 'fs';
 
-const t = c => {
-        const d = x => x ? parseInt(x) : 0;
-        const [, h, m, s] = /(\d+h)?(\d+m)?(\d+s)?/i.exec(c);
-        return (d(h) * 60 + d(m)) * 60 + d(s)
-    };
-const [, h, s] = /https?:\/\/youtu\.be\/([^?]+)\?t?=?(.+)/i.exec(prompt("Введите ссылку на видео"));
-const e = prompt(`Время окончания фрагмента (должно быть больше, чем ${s})\n, можно оставить пустым`);
+const fs = require('fs');
+const path = require('path');
 
-void prompt("Вот она ваша ссылка:", `https://www.youtube.com/embed/${h}?start=${t(s)}&end=${t(e)}&autoplay=1`)
+const filePath = path.join(__dirname, '../package.json');
+
+// console.log(filePath);
+
+fs.readFile(filePath, {encoding: 'utf-8'}, (err, data) => {
+    if (!err) {
+        console.log(`received data: ${data}`);
+
+        // JSON parsing
+        const json = JSON.parse(data);
+        const {
+            devDependencies: {"babel-cli": babelCli},
+            scripts: {run}
+        } = json;
+        console.log(babelCli);
+        console.log(run);
+
+        // File lines read
+        const lines = data.split('\n');
+        for (let line of lines) {
+            console.log(line.substr(0, 15));
+        }
+    } else
+        console.log(err);
+});
